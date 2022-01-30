509e598060a8a535d4ee1f8bf769bbc45dcc8c45
Kavya <kavyakaruppusamy09@gmail.com>
2022-01-30 02:33:56 -0600

    for loops

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-console.log(typeof(1));
-console.log(typeof(1.1));  //number
-console.log(typeof('1.1')); //string
-console.log(typeof(true));   //Boolean
-console.log(typeof(null));    //object
-console.log(typeof(undefined));  //undefined
-console.log(typeof([]));  //object
-console.log(typeof({})); //object
-console.log(typeof(NaN)); // number
+// 1) For the given JSON iterate over all for loops (for, for in, for of, forEach)
+
+
+var object = [
+    { "person": "Mohan", "age": 2, "company": "Guvi" },
+    { "person": "Rohan", "age": 3, "company": "Guvi Geeks" },
+    { "person": "Raju", "age": 4, "company": "Guvi Geek Network" },
+  ];
+//normal for loop
+// for(let i=0;i<object.length;i++){
+// console.log(`Person:${object[i].person}
+// age: ${object[i].age}
+// company:${object[i].company}
+// `)
+// }
+
+//for in loop
+// for(let i in object){
+// console.log(`Person:${object[i].person}
+// age: ${object[i].age}
+// company:${object[i].company}
+// `)
+// }
+
+//forEach loop
+// object.forEach((item) =>{
+//     console.log(`Person:${item.person}
+// age: ${item.age}
+// company:${item.company}
+// `)
+// });
+
+//for of loop
+for(let i of object) {
+console.log(`Person:${i.person}
+age: ${i.age}
+company:${i.company}
+`)
+}
+
+
+    
+       
+   
+    
+    
